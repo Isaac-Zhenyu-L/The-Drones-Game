1ced68edad5b3ea8a2b2b724f49c240db31578d8
lzyk9999 <lzyk9999@gmail.com>
2022-10-28 00:23:06 +1030

    updated

--- coordinate.js
+++ coordinate.js
@@ -11,81 +11,78 @@ const getCoordinate = (movingCommander) => {
 			_directionIndex.push([i, movingCommander.charAt(i)]);
 		}
 	}
-	console.log(_directionIndex);
 	_directionIndex.forEach((direction, index) => {
-		// directions.forEach((direction, direction_index) => {
 		if (index == 0) {
+			if (_destinationPosition[2] === "N") {
+				_result[1] = _result[1] + _directionIndex[index + 1][0];
+			}
+			if (_destinationPosition[2] === "S") {
+				_result[1] = _result[1] - _directionIndex[index + 1][0];
+			}
+			if (_destinationPosition[2] === "W") {
+				_result[0] = _result[0] - _directionIndex[index + 1][0];
+			}
+			if (_destinationPosition[2] === "E") {
+				_result[0] = _result[0] + _directionIndex[index + 1][0];
+			}
+		}
+		if (index == _directionIndex.length - 1) {
+			let currentMove = direction[0] - _directionIndex[index - 1][0] - 1;
+			if (_result[2] === "N") {
+				_result[1] = _result[1] + currentMove;
+			}
+			if (_result[2] === "S") {
+				_result[1] = _result[1] - currentMove;
+			}
+			if (_result[2] === "W") {
+				_result[0] = _result[0] - currentMove;
+			}
+			if (_result[2] === "E") {
+				_result[0] = _result[0] + currentMove;
+			}
+			console.log("length", _result);
+		} else {
+			let currentMove = _directionIndex[index + 1][0] - direction[0] - 1;
+			console.log("no length", _result);
 			if (direction[1] === "R") {
-				if (_destinationPosition[2] === "N") {
+				if (_result[2] === "N") {
 					_result[2] = "E";
+					_result[0] = _result[0] + currentMove;
 				}
-				if (_destinationPosition[2] === "S") {
-					_result[2] = "W ";
+				if (_result[2] === "S") {
+					_result[2] = "W";
+					_result[0] = _result[0] - currentMove;
 				}
-				if (_destinationPosition[2] === "W") {
+				if (_result[2] === "W") {
 					_result[2] = "N";
+					_result[1] = _result[1] + currentMove;
 				}
-				if (_destinationPosition[2] === "E") {
+				if (_result[2] === "E") {
 					_result[2] = "S";
+					_result[1] = _result[1] - currentMove;
 				}
 			}
 			if (direction[1] === "L") {
-				if (_destinationPosition[2] === "N") {
+				if (_result[2] === "N") {
 					_result[2] = "W";
+					_result[0] = _result[0] - currentMove;
 				}
-				if (_destinationPosition[2] === "S") {
+				if (_result[2] === "S") {
 					_result[2] = "E";
+					_result[0] = _result[0] + currentMove;
 				}
-				if (_destinationPosition[2] === "W") {
+				if (_result[2] === "W") {
 					_result[2] = "S";
+					_result[1] = _result[1] - currentMove;
 				}
-				if (_destinationPosition[2] === "E") {
+				if (_result[2] === "E") {
 					_result[2] = "N";
+					_result[1] = _result[1] + currentMove;
 				}
+				console.log("no length", _result);
 			}
-		} else {
-			let currentMove = direction[0] - _directionIndex[index - 1][0] - 1;
-			console.log("_result", direction[1], currentMove);
-			if (direction[1] === "R") {
-				if (_destinationPosition[2] === "N") {
-					_result[2] = "E";
-					_result[0] = _destinationPosition[0] + currentMove;
-					console.log("_result", _result);
-				}
-				// if (_destinationPosition[2] === "S") {
-				// 	_result[2] = "W ";
-				// 	_result[0] = _destinationPosition[0] - currentMove;
-				// }
-				// if (_destinationPosition[2] === "W") {
-				// 	_result[2] = "N";
-				// 	_result[1] = _destinationPosition[1] + currentMove;
-				// }
-				// if (_destinationPosition[2] === "E") {
-				// 	_result[2] = "S";
-				// 	_result[1] = _destinationPosition[1] - currentMove;
-				// }
-			}
-			// if (direction[1] === "L") {
-			// 	if (_destinationPosition[2] === "N") {
-			// 		_result[2] = "W";
-			// 		_result[0] = _destinationPosition[0] - currentMove;
-			// 	}
-			// 	if (_destinationPosition[2] === "S") {
-			// 		_result[2] = "E";
-			// 		_result[0] = _destinationPosition[0] + currentMove;
-			// 	}
-			// 	if (_destinationPosition[2] === "W") {
-			// 		_result[2] = "S";
-			// 		_result[1] = _destinationPosition[1] - currentMove;
-			// 	}
-			// 	if (_destinationPosition[2] === "E") {
-			// 		_result[2] = "N";
-			// 		_result[1] = _destinationPosition[1] + currentMove;
-			// 	}
-			// }
 		}
-		// });
 	});
-	// console.log(_result);
+	console.log("final", _result);
 };
 module.exports.getCoordinate = getCoordinate;
